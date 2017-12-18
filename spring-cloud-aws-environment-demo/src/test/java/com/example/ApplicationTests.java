ff32d558595315786f76bfc4555b57c94bf9991d
KennyK <kenkrueger65@gmail.com>
2017-12-18 14:17:33 -0500

    Upgraded from old spotify maven plugin to new plugin

--- spring-cloud-aws-environment-demo/src/test/java/com/example/ApplicationTests.java
+++ spring-cloud-aws-environment-demo/src/test/java/com/example/ApplicationTests.java
@@ -1,14 +1,13 @@
 package com.example;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootConfiguration()
 @WebAppConfiguration
 public class ApplicationTests {
 
