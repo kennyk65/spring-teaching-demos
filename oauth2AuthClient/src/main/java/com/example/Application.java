5e43fa883378e2d6ef4ba3d62d64aeb5a461257b
Ken Krueger <kenkrueger65@gmail.com>
2018-05-28 21:07:39 -0400

    cleanup

--- oauth2AuthClient/src/main/java/com/example/Application.java
+++ oauth2AuthClient/src/main/java/com/example/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableOAuth2Sso	//	Use OAuth2 for security
+//@EnableOAuth2Sso	//	No longer needed if you have a spring.security.oauth2.client.registration property
 public class Application {
 
 	public static void main(String[] args) {
