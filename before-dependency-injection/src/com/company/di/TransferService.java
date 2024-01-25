ca9670825c44f631a310eb184fc7313588342e2d
Ken Krueger <kenkrueger65@gmail.com>
2024-01-25 14:01:37 -0500

    latest

--- before-dependency-injection/src/com/company/di/TransferService.java
+++ before-dependency-injection/src/com/company/di/TransferService.java
@@ -5,10 +5,16 @@ public class TransferService {
     private AccountRepository accountRepository;
     private TransferRepository transferRepository;
 
+//    String accountNumber;
+
     public TransferService(AccountRepository ar, TransferRepository tr) {
         accountRepository = ar;
         transferRepository = tr;
     }
 
     // ... 
+
+    public void process(String accountNUmber) {
+        // do something with the acount number
+    }
 }
