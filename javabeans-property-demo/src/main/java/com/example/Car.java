18300d6bf5eb4a1201714eaefd734ef753dda596
Ken Krueger <kenkrueger65@gmail.com>
2024-04-01 10:13:54 -0400

    javabeans demo

---
+++ javabeans-property-demo/src/main/java/com/example/Car.java
@@ -0,0 +1,38 @@
+package com.example;
+
+public class Car {
+
+    private int doors;
+    private String color;
+    private boolean electric;
+    private int hp;
+
+
+    public int getHorspower() {
+        return hp;
+    }
+    public void setHorspower(int horspower) {
+        this.hp = horspower;
+    }
+    public int getDoors() {
+        return doors;
+    }
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public boolean isElectric() {
+        return electric;
+    }
+    public void setElectric(boolean electric) {
+        this.electric = electric;
+    }
+
+    
+
+}
